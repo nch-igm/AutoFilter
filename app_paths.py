ad4b59ad1b1036bcb7a64be5e0b7d9bce3a9f511
Gaither, Jeffrey <Jeffrey.Gaither@nationwidechildrens.org>
2020-03-25 10:50:02 -0400

    Initial commit

---
+++ app_paths.py
@@ -0,0 +1,11 @@
+
+
+## The only required paths are bedtools_app, happy_app, tabix_app 
+## and the three autofilt_ apps. 
+cmd_dict={
+    'bedtools_app':'/igm/apps/bedtools/bedtools2/bin/bedtools',
+    'happy_app':'/igm/apps/hap.py/bin/hap.py',
+    'gatk38_app':'/igm/apps/gatk/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar',
+    'tabix_app':'tabix',
+    }
+
